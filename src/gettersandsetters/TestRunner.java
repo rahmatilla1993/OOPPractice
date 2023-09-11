34fae67a47bd5bf9577a8020f3fbed05fedbcde7
rahmatilla301993 <rahmatilla1993@gmail.com>
2023-09-11 12:33:49 +0500

    oop examples1

---
+++ src/gettersandsetters/TestRunner.java
@@ -0,0 +1,8 @@
+package gettersandsetters;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        var student = new Student("John", "Beruni st.", "IT", 22, 1555.23);
+        System.out.println(student);
+    }
+}
