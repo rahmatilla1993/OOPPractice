2b2bf9c08d71b0131a5471214cc08c3e6e18deb0
rahmatilla301993 <rahmatilla1993@gmail.com>
2023-09-11 08:57:15 +0500

    method overload

---
+++ src/overloads/MathFunctions.java
@@ -0,0 +1,99 @@
+package overloads;
+
+public class MathFunctions {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static long add(long a, long b) {
+        return a + b;
+    }
+
+    public static float add(float a, float b) {
+        return a + b;
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static long sub(long a, long b) {
+        return a - b;
+    }
+
+    public static float sub(float a, float b) {
+        return a - b;
+    }
+
+    public static double sub(double a, double b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static long mul(long a, long b) {
+        return a * b;
+    }
+
+    public static float mul(float a, float b) {
+        return a * b;
+    }
+
+    public static double mul(double a, double b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+
+    public static long div(long a, long b) {
+        return a / b;
+    }
+
+    public static float div(float a, float b) {
+        return a / b;
+    }
+
+    public static double div(double a, double b) {
+        return a / b;
+    }
+
+    public static int getAbsoluteValue(int a) {
+        return Math.abs(a);
+    }
+
+    public static long getAbsoluteValue(long a) {
+        return Math.abs(a);
+    }
+
+    public static float getAbsoluteValue(float a) {
+        return Math.abs(a);
+    }
+
+    public static double getAbsoluteValue(double a) {
+        return Math.abs(a);
+    }
+
+    public static int getPow2(int a) {
+        return a * a;
+    }
+
+    public static long getPow2(long a) {
+        return a * a;
+    }
+
+    public static float getPow2(float a) {
+        return a * a;
+    }
+
+    public static double getPow2(double a) {
+        return a * a;
+    }
+}
